8ef4311e923c43e5982e9886d322e8ecaef460f5
godtoy <zhaojunlike@gmail.com>
2020-08-05 16:00:11 +0800

    a

--- src/main/java/com/oeynet/hackers/plugin/utils/DeviceUtils.java
+++ src/main/java/com/oeynet/hackers/plugin/utils/DeviceUtils.java
@@ -3,8 +3,11 @@ package com.oeynet.hackers.plugin.utils;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
+import android.os.Build;
 import android.telephony.TelephonyManager;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
 
 /**
  * Created by ZengYinan.
@@ -13,6 +16,7 @@ import android.telephony.TelephonyManager;
  * Desc:
  */
 public class DeviceUtils {
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public static String getIMEI(Context context) {
         try {
             //实例化TelephonyManager对象
@@ -21,7 +25,7 @@ public class DeviceUtils {
             if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
                 return "";
             }
-            String imei = telephonyManager.getDeviceId();
+            String imei = telephonyManager.getImei();
             //在次做个验证，也不是什么时候都能获取到的啊
             if (imei == null) {
                 imei = "";
